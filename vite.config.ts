a4c9443e6a719f3eec6e2330043dfc962cf76bec
oskygon <oskygon@yahoo.es>
2025-03-18 09:27:24 -0300

    cambios en package json y vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/gestion-pacientes-medicina/", // para github
   server: {
     host: "::",
     port: 8080,
